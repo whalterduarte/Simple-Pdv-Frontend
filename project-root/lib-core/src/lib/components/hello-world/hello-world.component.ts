50e7940ec879b884c3f5d878a397583f5816cecc
Gabiroba <gabrielcardosogirarde@gmail.com>
2024-07-11 22:32:45 -0300

    chore: Estruturando projeto

---
+++ project-root/lib-core/src/lib/components/hello-world/hello-world.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-hello-world',
+  standalone: true,
+  imports: [],
+  templateUrl: './hello-world.component.html',
+  styleUrl: './hello-world.component.css'
+})
+export class HelloWorldComponent {
+
+}
--- project-root/lib-core/src/public-api.ts
+++ project-root/lib-core/src/public-api.ts
@@ -1,3 +1,5 @@
 /*
  * Public API Surface of lib-core
  */
+
+export { HelloWorldComponent } from "./lib/components/hello-world/hello-world.component";
